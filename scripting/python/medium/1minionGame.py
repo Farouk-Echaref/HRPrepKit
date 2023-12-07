3441f3555ae279c7f316496208dddf5ae949a9e5
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2023-12-07 19:45:44 +0100

    feat: substrings out of a string

---
+++ scripting/python/medium/1minionGame.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+# the most straight forward method: 
+def is_vowel(c) -> bool:
+    vowels = "aeuioAEUIO"
+    if c in vowels:
+        return True
+    return False
+
+
+def create_substring(charList: list, ln: int) -> list:
+    substring = [charList[i: i + ln] for i in range(0, len(charList))]
+    return substring
+
+def num_of_occurences(substring: str, main_string: str) -> int:
+    count: int = 0
+    start_index: int = 0
+    while (start_index < len(main_string)):
+        index = main_string.find(substring, start_index)
+
+        if (index == -1):
+            return count
+        
+        count += 1
+        start_index = index + 1
+
+    return count
+
+def check_substring(totalSubs: list, string: str) -> None:
+    scoreMap = {"Stuart": 0,
+                "Kevin": 0
+    }
+
+    for sub in totalSubs:
+        newStr = ''.join(sub)
+        if newStr in string:
+            if is_vowel(sub[0]) == True:
+                scoreMap["Kevin"] += num_of_occurences(newStr, string)
+            else:
+                scoreMap["Stuart"] += num_of_occurences(newStr, string)
+
+    if (is_vowel(string[0]) == True):
+        scoreMap["Kevin"] += 1
+    else:
+        scoreMap["Stuart"] += 1
+
+    if (scoreMap["Kevin"] > scoreMap["Stuart"]):
+        print("Kevin", scoreMap["Kevin"])
+    elif (scoreMap["Kevin"] < scoreMap["Stuart"]):
+        print("Stuart", scoreMap["Stuart"])
+    elif (scoreMap["Kevin"] == scoreMap["Stuart"]):
+        print("Draw")
+
+def minion_game(string: str) -> None:
+    totalSubs = []
+    charList = list(string)
+    
+    for i in range(1, len(charList)):
+        newSub = tuple(create_substring(charList, i))
+        for ls in newSub:
+            if ls not in totalSubs:
+                totalSubs.append(ls)
+    
+    check_substring(totalSubs, string)
+
+if __name__ == '__main__':
+    myStr = input()
+    minion_game(myStr)
