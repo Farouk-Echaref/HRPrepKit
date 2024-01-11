14154361f3287d54e5fa59b4264bf4f1041ea80d
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2024-01-11 16:01:56 +0100

    learning python classes: class attrs vs instance attrs

--- scripting/python/python_classes/1class.py
+++ scripting/python/python_classes/1class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-class testing:
+class Testing:
     def __init__(self, name: str, age: int, pos: str) -> None:
         self.name = name
         self.age = age
@@ -8,16 +8,33 @@ class testing:
     def print_attr(self) -> None:
         print(self.name, self.age, self.pos)
 
-class second:
+class Second:
     def __init__(self, val: int) -> None:
         self.val = val
     def getVal(self) -> int:
         return (self.val)
+    def printVal(self) -> None:
+        print("my val is {}".format(self.val))
+
+class Fech:
+    #class attribute
+    name = "farouk"
+
+    #instance attribute
+    def __init__(self, login : str) -> None:
+        self.login = login
 
 
 if __name__ == "__main__":
-    obj = testing("fech", 24, "intern")
-    sc = second(42)
+    obj = Testing("fech", 24, "intern")
+    sc = Second(42)
+    fch = Fech("fech-cha")
 
     obj.print_attr()
-    print(sc.getVal())
+    sc.printVal()
+
+    #accessing class attributes
+    print(fch.__class__.name)
+
+    #accessing instance attributes
+    print(fch.login)
